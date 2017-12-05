9fb6bfe616b0f08bcff2b33b3bf684af36f8e3de
Unknown <sapnabapat270@gmail.com>
2017-12-05 17:12:58 +0530

    first push of redux app

---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,16 @@
+import * as types from '../actions/actionTypes';
+
+
+export default function courseReducer(state=[],action){
+  switch(action.type){
+    case types.LOAD_COURSES_SUCCESS:
+      /*return [...state,
+      Object.assign({},action.course)
+      ];*/
+
+      return action.courses;
+
+    default:
+      return state;
+  }
+}
